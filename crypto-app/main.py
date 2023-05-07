ddf9f2d9feabc38670624b28f4f9a44ed4345371
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-05-07 16:57:01 +0300

    add database.py and models.py

--- crypto-app/main.py
+++ crypto-app/main.py
@@ -4,9 +4,8 @@ from database import engine
 from routers import crypto
 from starlette.staticfiles import StaticFiles
 
-app = FastAPI
+app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(crypto.routers)
---
+++ crypto-app/models.py
@@ -0,0 +1,14 @@
+from database import Base
+from sqlalchemy import Column, String, Integer
+
+
+# users table
+class Users(Base):
+
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    password = Column(String)
