2b017b588a401ee6b22e65d2ddce354c09bd1bfa
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-05-19 16:09:16 +0300

    add markets endpoint

--- crypto-app/main.py
+++ crypto-app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 import models
 from database import engine
-from routers import assets
+from routers import assets, exchanges, markets, rates, auth
 from fastapi.staticfiles import StaticFiles
 
 
@@ -9,4 +9,8 @@ app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-app.include_router(crypto.routers)
+app.include_router(assets.routers)
+app.include_router(auth.routers)
+app.include_router(exchanges.routers)
+app.include_router(markets.routers)
+app.include_router(rates.routers)
---
+++ crypto-app/routers/markets.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.orm import Session
+from starlette import status
+from starlette.responses import HTMLResponse, RedirectResponse
+from starlette.templating import Jinja2Templates
+
+from database import SessionLocal
+from routers.auth import get_current_user
+
+routers = APIRouter(
+    prefix="/markets",
+    tags=["markets"],
+    responses={404: {"description": "Not found!"}}
+)
+
+templates = Jinja2Templates(directory="templates")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@routers.get("/markets", response_class=HTMLResponse)
+async def get_all_markets(request: Request, db: Session = Depends(get_db)):
+
+    user = get_current_user(request)
+
+    if user is None:
+        return RedirectResponse('/auth', status_code=status.HTTP_302_FOUND)
+
+    # add init for API
+
+    return templates.TemplateResponse('', {"request": request, "user": user})
